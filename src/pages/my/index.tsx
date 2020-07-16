99ab6437f51837a928409e82df99f498c96c154b
gongjian <gongjian@woda.ink>
2020-07-16 19:30:04 +0800

    submit

--- src/pages/my/index.tsx
+++ src/pages/my/index.tsx
@@ -2,7 +2,11 @@ import React, { Component } from 'react'
 import { View, Text, Image, Input } from '@tarojs/components'
 import Taro from '@tarojs/taro'
 import myPageBg1 from '../../images/myPageBg1.png'
-import toDetail from '../../images/toDetail.png'
+import arrow_right2 from '../../images/arrow_right2.png'
+import avatar from '../../images/avatar.png'
+import aboutIcon from '../../images/aboutIcon.png'
+import accountIcon from '../../images/accountIcon.png'
+import arrow_right3 from '../../images/arrow_right3.png'
 import './index.less'
 
 export default class Index extends Component {
@@ -19,13 +23,45 @@ export default class Index extends Component {
   componentDidHide () { }
 
   render () {
+    const json = [
+      {
+        icon: aboutIcon,
+        name: '关于',
+        router: 'pages/about/index'
+      },
+      {
+        icon: accountIcon,
+        name: '账号管理',
+        router: 'pages/acount/index'
+      }
+    ]
     return (
       <View className='indexPage'>
         <View className='header'>
           <Image src={myPageBg1} className='myPageBg1'></Image>
-          <View className='userInfoView'></View>
+          <View className='userInfoView'>
+            <Image src={avatar} className='avatar'></Image>
+            <View className='toLogin'>您好，请登录</View>
+            <Image src={arrow_right2} className='arrow_right2'></Image>
+          </View>
         </View>
-        <View className='mainContent'></View>
+        <View className='mainContent'>
+        {
+            json.length > 0 && json.map((item, index) => {
+              return (
+                <View className='listView'>
+                  <Image src={item.icon} className='icon'></Image>
+                  <View className='name'>{item.name}</View>
+                  <Image src={arrow_right3} className='arrow_right3'></Image>
+                  {
+                    index + 1 < json.length &&
+                    <View className="splitLine"></View>
+                  }
+                </View>
+              )
+            })
+        }
+        </View>
       </View>
     )
   }
